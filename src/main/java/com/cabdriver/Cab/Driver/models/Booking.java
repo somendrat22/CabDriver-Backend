036ae1abb4e4c8b30d5da164a7682cade310079c
Somendrat22 <somendrat22@gmail.com>
2024-06-28 22:53:11 +0530

    Login and signup logic done

--- src/main/java/com/cabdriver/Cab/Driver/models/Booking.java
+++ src/main/java/com/cabdriver/Cab/Driver/models/Booking.java
@@ -1,12 +1,17 @@
 package com.cabdriver.Cab.Driver.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Booking {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
     @ManyToOne
     Customer customer;
@@ -15,64 +20,4 @@ public class Booking {
     String feedback;
     @ManyToOne
     Driver driver;
-
-    public Booking() {
-    }
-
-    public Booking(int id, Customer customer, int billAmount, String status, String feedback, Driver driver) {
-        this.id = id;
-        this.customer = customer;
-        this.billAmount = billAmount;
-        this.status = status;
-        this.feedback = feedback;
-        this.driver = driver;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public int getBillAmount() {
-        return billAmount;
-    }
-
-    public void setBillAmount(int billAmount) {
-        this.billAmount = billAmount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
-
-    public Driver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(Driver driver) {
-        this.driver = driver;
-    }
 }
---
+++ src/main/java/com/cabdriver/Cab/Driver/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.cabdriver.Cab.Driver.repository;
+
+import com.cabdriver.Cab.Driver.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+}
